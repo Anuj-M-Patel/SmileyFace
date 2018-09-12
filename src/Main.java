0769f1ef6ed21e4fa0b73d90137c0aac13d18ac5
Anuj-M-Patel <42977181+Anuj-M-Patel@users.noreply.github.com>
2018-09-12 13:02:43 -0400

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ * @author Anuj Patel
+ * Smiley Face Project
+ */
+
+public class Main 
+{
+
+	public static void main(String[] args) 
+	{
+
+		System.out.print("\\\\\\\\\\\\\\\n| * * |\n|  *  |\n| \\_/ |\n\\_____/");
+
+	}
+
+}
